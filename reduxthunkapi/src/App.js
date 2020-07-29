f2c1dd615d6ee75ae963446e461d91837d11bae6
Brandon Stine <hopeful89@Brandons-MacBook-Pro.local>
2020-07-29 11:40:03 -0400

    state change for ui fetch button

--- reduxthunkapi/src/App.js
+++ reduxthunkapi/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import "./App.css";
-import { fetchTemtemStart } from './actions/defaultActions'
+import { fetchTemtemStart } from "./actions/defaultActions";
 
 const App = (props) => {
+  console.log("bs: APP.js: propsFromState", props);
   return (
     //write map to state with incoming information from api
     //decide on a styling library and work on this project
     //whiteboard out a flow and practice
     <div className="App">
-      <button onClick={props.fetchTemtemStart}>test button</button>
+      {props.isFetching ? (//removes button during fetch
+        <div>**Please wait**</div>
+      ) : (
+        <button onClick={() => props.fetchTemtemStart()}>test button</button>
+      )}
     </div>
   );
-}
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    temtem: state.temtem
-  }
-}
+    temtem: state.temtem,
+    isFetching: state.isFetching,
+  };
+};
 
-export default connect(mapStateToProps,{ fetchTemtemStart })(App);
+export default connect(mapStateToProps, { fetchTemtemStart })(App);
--- reduxthunkapi/src/reducers/ruducer.js
+++ reduxthunkapi/src/reducers/ruducer.js
@@ -12,11 +12,17 @@ export const defaultReducer = (state = initialState, action) => {
             console.log('test test test', action)
             //TODO: 
             // need to update state with isFetching as true and pass to UI with an update during fetch
-            return state;
+            return {
+                ...state, 
+                isFetching: true,
+            };//removes button during fetch
         case FETCH_TEMTEM_SUCCESS:
             console.log('this is the success being called')
             //TODO: update state with new data from api call look for this information in mapToProps
-            return state;
+            return {
+                ...state,
+                isFetching: false,
+            };
         case FETCH_TEMTEM_FAILURE:
             //TODO: update state with error message from action.payload and display on screen
             console.log('this is a failure being called')
